e8143b99e2210f97ca1c4223017eb5e849e407d6
George Connor <grgcnnr@gmail.com>
2013-08-27 09:50:51 +1200

    Updated Policy object with url fragment field and short dec

--- mysite/code/Policy.php
+++ mysite/code/Policy.php
@@ -8,6 +8,8 @@
 
     public static $db = array(
       'Name'=>'Varchar',  
+      'UrlFragment'=>'Varchar',
+      'Summary'=>'Text',
       'Details'=>'HTMLText',
 
     );
@@ -19,6 +21,21 @@
     public static $belongs_many_many=array(
         'PolicyPages'=>'PolicyPage'
     );
+
+    //Fields for the DOM Popup
+    public function getCMSFields() {
+      $fields = parent::getCMSFields();
+
+      $fields->addFieldToTab("Root.Main", new TextField('Name') );
+      $fields->addFieldToTab("Root.Main", $frag = new TextField('UrlFragment', 'URL Fragment' ) );
+        $frag->setRightTitle( 'The url fragment is used to set the Policy section ID on the pocicies page so "/stoke" can redirect  to "#stoke" ');
+
+      $fields->addFieldToTab("Root.Main", $sum = new TextareaField('Summary') );
+        $sum->setRightTitle('A short description of the policy for the homepage');
+      $fields->addFieldToTab("Root.Main", new HTMLEditorField('Details') );
+
+      return $fields;
+    }
 
   }
 
--- themes/ml/templates/Layout/HomePage.ss
+++ themes/ml/templates/Layout/HomePage.ss
@@ -1,22 +1,22 @@
-      <!-- Example row of columns -->
-      <div class="row clearfix">
-        <div class="span6">
-         $Content  
-        </div>
+<!-- Example row of columns -->
+<div class="row clearfix">
+  <div class="span6">
+   $Content  
+  </div>
 
-        <div class="span6 ">
-          <ul class="homepage-policies">
-            <% loop $Policies %> 
-            <li class="homepage-policy">
-            <h3 class="homepage-policy-title">$Name</h3>
-            <div class="homepage-policy-details">
-             $Details
-            </div>
-            </li>
-            <% end_loop %>       
-          </ul>
-        </div>
-	
+  <div class="span6 ">
+    <ul class="homepage-policies">
+      <% loop $Policies %> 
+      <li class="homepage-policy">
+      <h3 class="homepage-policy-title">$Name</h3>
+      <div class="homepage-policy-details">
+       $Details
       </div>
+      </li>
+      <% end_loop %>       
+    </ul>
+  </div>
+
+</div>
      
 
